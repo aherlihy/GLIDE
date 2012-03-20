ea972565ba044e5bef90fe78188a63d5a41e9e63
Anna Herlihy <anna_herlihy@brown.edu>
2012-03-20 19:08:36 -0400

    final demo

---
+++ sandboxDemo/test_runtime_error.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python
+
+# This program is a template for a program run through the sandbox.
+# For convenience, use this class to try various runtime exceptions.
+
+# For each type of exception, uncomment the line and pass this path into sBox.py
+
+# Simple examples:
+
+#zero = 0
+#num = 100
+#kaboom = num/zero
+
+#list = [10,20,30]
+#kaboom = list[3]
+
+# To test more unpredictable exceptions, throw by hand using raise <exception>
+# Simple examples: 
+
+#raise BufferError
+#raise EnvironmentError
+#raise OSError
