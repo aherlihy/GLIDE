1cba3891d34b79ce3788bae04725ac9d66cf646e
Anna Herlihy <anna_herlihy@brown.edu>
2012-04-29 16:19:02 -0400

    fixed parsing

--- sandbox/output.py
+++ sandbox/output.py
@@ -1,2 +1,7 @@
-NO ERRORS!
-Careful though - your code may be correct but not do exactly what you want
+Oh no! Looks like you've got an error in your code :(
+Check out the help pages if you're not sure how to fix it!
+
+RUNTIME ERROR
+ZeroDivisionError: integer division or modulo by zero
+line 6
+code: "10/0"
---
+++ sandbox/runLevel.py
@@ -0,0 +1,7 @@
+#!usr/bin/python
+from tilemap import *
+from avatar import *
+
+def runLevel(plane):
+    10/0
+    return
