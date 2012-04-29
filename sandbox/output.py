945673db7fedbc482e477680bdb05c4e0ba0047a
Anna Herlihy <anna_herlihy@brown.edu>
2012-04-29 18:01:05 -0400

    oops

--- sandbox/output.py
+++ sandbox/output.py
@@ -1,2 +1,7 @@
-NO ERRORS!
-Careful though - your code may be correct but not do exactly what you want
+Oh no! Looks like you've got an error in your code :(
+Check out the help pages if you're not sure how to fix it!
+
+RUNTIME ERROR
+TypeError: move() takes exactly 1 argument (2 given)
+line 9
+code: "plane.move(1)"
