3997e271b44aabbc5046850d74b141c7cbf8eac2
John Oberbeck <jwoberbe@cs.brown.edu>
2012-05-06 14:29:38 -0400

    teacher's desk

--- sandbox/tilemap.py
+++ sandbox/tilemap.py
@@ -61,8 +61,8 @@ class Tile(object):
     """
 
     def __init__(self, value= "AIR") :
-        self.valid = ["AIR","ISLAND","WALL","GATE","PLANE","DESK","BRICK","STUDENT"]
-        self.bitval = ["A","I","W","G","P","?","B","S"]
+        self.valid = ["AIR","ISLAND","WALL","GATE","PLANE","DESK","BRICK","STUDENT","TEACHER"]
+        self.bitval = ["A","I","W","G","P","?","B","S","T"]
         self.six = ["0","1","2","3","4","5","6","7","8","9","a",
                 "b","c","d","e","f"]
         self.sixes = ["0000","0001","0010","0011","0100","0101","0110",
