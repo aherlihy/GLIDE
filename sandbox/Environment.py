60a5a259f15bc32a9c59479a0e21648bc871fd25
Anna Herlihy <anna_herlihy@brown.edu>
2012-05-08 13:28:06 -0400

    level1

--- sandbox/Environment.py
+++ sandbox/Environment.py
@@ -72,7 +72,7 @@ class Environment(Frame):
         self.initTextBoxes()
         self.initUI()
 
-        self.currLevel = 6
+        self.currLevel = 1
 
         self.beatenLevels = []
         self.initLevelCanvas()
